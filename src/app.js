e05ba74d3d1a1f376e4d1e5c62833d10171c1b9b
나다운 <skekfl12@gitauto.com>
2023-02-14 11:42:58 +0900

    Create News Feed

--- src/app.js
+++ src/app.js
@@ -2,4 +2,45 @@ import ajax from "./http";
 
 const NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 
-console.log(ajax("GET", NEWS_URL, false));
+const app = document.querySelector("#root");
+
+window.addEventListener("load", init);
+
+/** Init */
+function init() {
+  createFeed();
+}
+
+/** Create News Feed */
+function createFeed() {
+  const newsFeed = ajax("GET", NEWS_URL, false);
+  let template = `
+    <section>
+      <h1>News Feed</h1>
+      <ul>
+        {{__news_feed__}}
+      </ul>
+      <div>
+        <span>
+          <a href="#">To Prev</a>
+          <a href="#">To Next</a>
+        </span>
+      </div>
+    </section>
+  `;
+
+  const newsList = [];
+  for (let i = 0; i < 30; i++) {
+    newsList.push(`
+      <li>
+        <a href="#${newsFeed[i].id}">
+          ${newsFeed[i].title}
+        </a>
+      </li>
+    `);
+  }
+
+  template = template.replace("{{__news_feed__}}", newsList.join(""));
+
+  app.innerHTML = template;
+}
