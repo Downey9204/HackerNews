f4e5a9487e6315e2bc63861d18661b6ba63f8c1a
나다운 <skekfl12@gitauto.com>
2023-02-27 11:21:49 +0900

    Restart project

--- src/http.js
+++ src/http.js
@@ -1,10 +1,1 @@
-const ajax = new XMLHttpRequest();
-
-const getData = (method, url, isAsync) => {
-  ajax.open(method, url, isAsync);
-  ajax.send();
-
-  return JSON.parse(ajax.response);
-};
-
-export default getData;
+console.log("http");
